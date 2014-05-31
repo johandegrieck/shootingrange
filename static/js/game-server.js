4d5a36d2bfcda2a0dcfb16bcad7f536caa89d877
Johan Degrieck <johneke@hotmail.com>
2014-06-01 00:50:13 +0200

    Update game-server.js
    socket io config for openshift compatibility

--- static/js/game-server.js
+++ static/js/game-server.js
@@ -1,6 +1,8 @@
 jQuery(function($){
 
-	var socket = io.connect();
+	/*var socket = io.connect();*/
+	/* openshift config */
+	var socket = io.connect("http://shootingrange-shootingrange.rhcloud.com:8000");
 	var $body = $('body');
 	var $initserver = $('#initserver');
 	
