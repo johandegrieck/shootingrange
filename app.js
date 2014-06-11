4cb099edbd637c3a27215cacf086c61c51973c06
johandegrieck <johandegrieck@hotmail.com>
2014-06-11 20:48:35 +0200

    server listen

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ var express = require('express'),
 /*SPECIFIC CONFIG FOR OPENSHIFT*/
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
+server.listen(app.get('port'), app.get('ipaddr'), function(){
+	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+});
 /*END OPENSHIFT SPECIFIC CODE */
 
-server.listen(3000);
+// port initialization on local machine
+//server.listen(3000);
 
 
 app.get('/', function(req,res){
