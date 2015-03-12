942fa4be0c48d29163ff55672b543ee8379ee1d0
Johan Degrieck <johand@hakka.eu>
2015-03-13 00:34:45 +0100

    server address adjusted

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var express = require('express'),
 	playerColors = ["player-color-1","player-color-2","player-color-3","player-color-4"],
 	playerCounter = 0;
 	
-/*SPECIFIC CONFIG FOR OPENSHIFT*/
+/*SPECIFIC CONFIG FOR OPENSHIFT
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
 server.listen(app.get('port'), app.get('ipaddr'), function(){
 	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
 });
-/*END OPENSHIFT SPECIFIC CODE */
+END OPENSHIFT SPECIFIC CODE */
 
 // port initialization on local machine
-//server.listen(3000);
+server.listen(3000);
 
 
 app.get('/', function(req,res){
